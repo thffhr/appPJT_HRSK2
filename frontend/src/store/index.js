090cde8682f4fc6dc0c9532b1b8733adbe04a107
psj8532 <psj8532@naver.com>
2020-10-21 11:30:26 +0900

    S03P31A307-66 PSJ: [ADD] Devtool

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducer/index';
-import userReducer from '../reducer/user';
+import { composeWithDevTools, devToolsEnhancer } from 'redux-devtools-extension';
 
-export const store = createStore(reducers);
+export const store = createStore(
+    reducers,
+    devToolsEnhancer(
+        reducers
+    )
+    // composeWithDevTools(
+    //     applyMiddleware(...middleware)
+    // )
+);
