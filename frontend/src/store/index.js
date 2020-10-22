fda9ae1b288b3cd14d2b318cac2a437d211e644a
psj8532 <psj8532@naver.com>
2020-10-22 12:39:10 +0900

    S03P31A307-73 PSJ: [ADD] init redux thunk

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducer/index';
 import { composeWithDevTools, devToolsEnhancer } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
 export const store = createStore(
     reducers,
-    devToolsEnhancer(
-        reducers
-    )
-    // composeWithDevTools(
-    //     applyMiddleware(...middleware)
+    // no middleware
+    // devToolsEnhancer(
+    //     reducers
     // )
+
+    // use middleware
+    composeWithDevTools(
+        applyMiddleware(ReduxThunk),
+    )
 );
